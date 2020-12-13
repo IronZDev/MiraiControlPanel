386b42d39e553069a9e1d0e30bbea8f7b18c5582
IronZaphilith <maciek_st@o2.pl>
2020-12-13 22:56:33 +0100

    Improved UI look Added disabling Attack button

--- public/javascripts/clientMain.js
+++ public/javascripts/clientMain.js
@@ -1,10 +1,27 @@
+let selectValid = false;
+let targetIPValid = false;
 //input mask bundle ip address
-$(document).ready(function() {
-    const ipv4_address = $('#targetIP');
-    ipv4_address.inputmask({
-        alias: "ip",
-        greedy: false //The initial mask shown will be "" instead of "-____".
-    });
+const targetIP = $('#targetIP');
+targetIP.inputmask({
+    alias: "ip",
+    greedy: false //The initial mask shown will be "" instead of "-____".
 });
 
-console.log("Loaded")
+function checkValidity() {
+    if(selectValid && targetIPValid) {
+        $('#attackBtn').removeAttr('disabled');
+    } else {
+        console.log("disable");
+        $('#attackBtn').attr('disabled', true);
+    }
+}
+
+$('#attackSelect').on('change', function() {
+    selectValid = this.value !== 0;
+    checkValidity();
+});
+
+targetIP.on('change', function() {
+    targetIPValid = !!this.value.match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/);
+    checkValidity();
+});
